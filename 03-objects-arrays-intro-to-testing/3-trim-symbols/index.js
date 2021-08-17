5756e3b67e21fbdef6ff6abcf7c53018d6b12826
Galagurya <olga.galagurya@gmail.com>
2021-08-17 17:46:17 +0300

    Implemented trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,32 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!string) {
+    return '';
+  }
 
+  if (size === 0) {
+    return '';
+  }
+
+  if (!size || size < 0) {
+    return string;
+  }
+
+  let current = string[0];
+  let count = 0;
+  let result = '';
+
+  for (let symbol of string) {
+    if (symbol !== current) {
+      count = 1;
+      result = result + symbol;
+      current = symbol;
+    } else if (count < size) {
+      count++;
+      result = result + symbol;
+    }
+  }
+
+  return result;
 }
