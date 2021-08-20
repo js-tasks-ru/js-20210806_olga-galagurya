a4c2c0a3542ec9d57959a10fc5f233eabed5dd3a
Galagurya <olga.galagurya@gmail.com>
2021-08-20 14:52:13 +0300

    Implement ColumnChart component

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,67 @@
 export default class ColumnChart {
+  constructor(options = {}) {
+    this.data = options.data ?? [];
+    this.label = options.label;
+    this.value = options.value;
+    this.link = options.link;
+    this.formatHeading = options.formatHeading;
+    this.chartHeight = options.chartHeight || 50;
+    this.render();
+  }
+
+  render() {
+    const element = document.createElement('div');
+    const viewAll = this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+    const isEmpty = !this.data.length ? 'column-chart_loading' : '';
+
+    element.innerHTML = `
+      <div class="column-chart ${isEmpty}" style="--chart-height: 50">
+      <div class="column-chart__title">${this.label}${viewAll}</div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">
+          ${this.formatValue(this.value)}
+        </div>
+        <div data-element="body" class="column-chart__chart">
+          ${this.renderColumns()}
+        </div>
+      </div>
+    </div>
+    `;
+
+    this.element = element.firstElementChild;
+  }
+
+  formatValue(value) {
+    return this.formatHeading ? this.formatHeading(value) : value;
+  }
+
+  renderColumns() {
+    const max = Math.max(...this.data);
+    const scale = 50 / max;
+    const columnChart = [];
+
+    for (let item of this.data) {
+      let percent = (item / max * 100).toFixed(0) + '%';
+      let val = String(Math.floor(item * scale));
+
+      columnChart.push(`<div style="--value: ${val};" data-tooltip="${percent}"></div>`);
+    }
+    return columnChart.join('');
+  }
+
+  update(data) {
+    this.data = data;
+    const bodyElement = this.element.querySelector('[data-element="body"]');
+
+    bodyElement.innerHTML = this.renderColumns();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 
 }
