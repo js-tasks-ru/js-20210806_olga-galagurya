6ac8ba9e1344e38af89d1a01452a9d4e41965139
Galagurya <olga.galagurya@gmail.com>
2021-08-27 10:17:41 +0300

    NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
+  /**
+   * @type {?NotificationMessage}
+   */
+  static current = null;
 
+  element;
+  timerId = null;
+
+  constructor(title = '', {duration = 1, type = ''} = {}) {
+    this.title = title;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.title}
+          </div>
+        </div>
+      </div>
+    `;
+
+    this.element = element.firstElementChild;
+  }
+
+  show(wrapper) {
+    if (NotificationMessage.current) {
+      NotificationMessage.current.remove();
+    }
+
+    NotificationMessage.current = this;
+    if (!wrapper) {
+      wrapper = document.querySelector('body');
+    }
+
+    wrapper.append(this.element);
+
+    this.timerId = setTimeout(() => this.remove(), this.duration);
+
+  }
+
+  remove() {
+    this.element.remove();
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+    }
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
